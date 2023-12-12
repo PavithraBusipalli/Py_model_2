cfd15d3bc144145eadbcd719b5d04d461f126be0
PavithraBusipalli <busipallipavitra@gmail.com>
2023-12-12 23:25:55 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -23,10 +23,69 @@ class Emp(models.Model):
         return self.ename
 
 
+'''
+# Data Insertion into Models: --> In 3 Ways
+1. By creation of obj directly : (O/P format is Obj)
+    e.g:
+        obj=Dept(deptno=50,dname=20,loc='hyd')
+    
+2. With the help of create():   (O/P format is Obj)
+    e.g:
+        obj=Dept.objects.create(deptno=30,dname=20,loc='hyd')
+
+3. With the help of get_or_create():    (O/P format is TUPLE (obj, bool)) --> bool is true if obj is not present else false
+    e.g:
+        >>> from app.models import *
+        >>> dept_obj=Dept.objects.create(deptno=40,dname='civil',loc='Noida')
+        >>> dept_obj
+        <Dept: civil>
+        >>> dept_obj1=Dept.objects.get_or_create(deptno=40)
+        >>> dept_obj1
+        (<Dept: civil>, False)
+'''
+
+
+'''
+# Data Retrieving methods:
+
+1.all()
+2. get()
+3. filter()
+
+1.all(): TO fetch entire data from the model
+    res=Model_name.objects.all()
+    e.g:a=
+         a=Dept.objects.all()
+        >>> a
+        <QuerySet [<Dept: cse>, <Dept: ECE>, <Dept: eee>, <Dept: civil>]>
+
+2.get(): To fetch one obj(row) at a time based on specified cond
+    res=Model_name.objects.get(field=val)
+    e.g:
+        a=Dept.objects.get(dname='cse')
+        >>> a
+        <Dept: cse>
+
+
+3.filter(): To fetch multiple or zero or one obj's(rows) at a time based on specified cond
+    res=Model_name.objects.get(filter_condition)
+    e.g:
+        a=Dept.objects.filter(deptno=10)
+        >>> a
+        <QuerySet [<Dept: cse>]>
+        >>> a=Emp.objects.filter(deptno=10)
+        >>> a
+        <QuerySet [<Emp: pavi>, <Emp: sam>]>
+
+
+'''
+
+
+
 # Bonus and Salgrade Table
 class Bonus(models.Model):
 
-    ename=models.CharField(max_length=10)
+    ename=models.OneToOneField(Emp,on_delete=models.CASCADE)
     job=models.CharField(max_length=10)
     sal=models.IntegerField()
     comm=models.IntegerField()
@@ -39,4 +98,4 @@ class Salgrade(models.Model):
     losal=models.IntegerField()
     hisal=models.IntegerField()
     def __str__(self):
-        return self.pk
+        return self.pkp
