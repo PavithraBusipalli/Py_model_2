1fe7ea89301fc6397b7f563464222de7f3ad39f9
PavithraBusipalli <busipallipavitra@gmail.com>
2023-12-13 18:19:07 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+
+def emp(request):
+    EQLO=Emp.objects.all()
+    d={'EQLO':EQLO}
+    return render(request,'display_emp_data.html',d)
+
+def dept(request):
+    DQLO=Dept.objects.all()
+    d={'DQLO':DQLO}
+    return render(request,'display_data.html',d)
